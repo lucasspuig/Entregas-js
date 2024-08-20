605d2555b015cc1f953e2d9046fee240719ad98d
lucaspuig <puiglucas82@gmail.com>
2024-08-20 17:32:10 -0300

    SegundaEntrega

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ for (let i = 0; i <= 0; i++) {
         break;
     }
 
+
+
+
+
+
     function dato1(nombreApellido) {
         console.log("Hola " + nombreApellido);
     }
@@ -19,67 +24,131 @@ for (let i = 0; i <= 0; i++) {
     dato1(nombreApellido);
 
 
-    let envio = 300;
-    let vip = 1000;
-    let ingreso = prompt("Seleccione con numero la cantidad de entradas ¨VIP¨ \n 0- Ninguna \n 1- Una entrada: $1000 \n 2- Dos entrada: $2000 \n 3- Tres entradas: $3000")
+let email = prompt("Ingrese direccion de correo electronico");
+
+console.log("Direccion de correo: " + email);
+
+
+
+
+
+    let envio = 3000;
+    let vip = 25000;
+    let ingreso = prompt("Seleccione con numero la cantidad de entradas ¨VIP¨ \n 0- Ninguna \n 1- Una entrada: $25.000 \n 2- Dos entrada: $50.000 \n 3- Tres entradas: $75.000")
     
     if (ingreso == "0" || ingreso == "cero" || ingreso == "CERO") {
         alert("Presiona aceptar para seleccionar tu  entrada General.");
     }
-    if (ingreso == "1" || ingreso == "uno" || ingreso == "UNO" ) {
+    else if (ingreso == "1" || ingreso == "uno" || ingreso == "UNO" ) {
         
         let resultadoVip1 = envio + vip;
         parseFloat(alert("Seleccionaste 1 Entrada Vip, valor mas envio: " + resultadoVip1));
-        console.log(resultadoVip1);
+        console.log("valor mas envio: " + resultadoVip1);
     }else if (ingreso == "2" || ingreso == "dos" || ingreso == "DOS") {
         
         let resultadoVip2 = vip * 2 + envio;
         parseFloat(alert("Seleccionaste Entrada Vip, valor mas envio: " + resultadoVip2));
-        console.log(resultadoVip2);
+        console.log("valor mas envio: " + resultadoVip2);
     } else if (ingreso == "3" || ingreso == "dos" || ingreso == "TRES") {
-        
-        let resultadoVip3 = vip * 3 + envio;
+        let totalVip3 = vip * 3 + envio;
+        let resultadoVip3 = totalVip3
         parseFloat(alert("Seleccionaste Entrada Vip, valor mas envio: " + resultadoVip3))
-        console.log(resultadoVip3);
+        console.log("valor mas envio: " + resultadoVip3);
     }else{
         alert("No seleccionaste ninguna opcion.")
+        break;
     }
+
+
+
+
+
+
+    let general = 15000;
+    let ingreso2 = prompt("Seleccione con numero la cantidad de entradas ¨GENERAL¨ \n 0- Ninguna \n 1- Una entrada: $15.000 \n 2- Dos entrada: $30.000 \n 3- Tres entradas: $45.000")
     
-    let general = 500;
-    let ingreso2 = prompt("Seleccione con numero la cantidad de entradas ¨GENERAL¨ \n 0- Ninguna \n 1- Una entrada: $500 \n 2- Dos entrada: $1000 \n 3- Tres entradas: $1500")
-    
-    if (ingreso == "0" || ingreso == "cero" || ingreso == "CERO") {
-        console.log("Error, vuelve a ingresar los datos para obtener una entrada");
+    if (ingreso2 == "0" || ingreso2 == "cero" || ingreso2 == "CERO") {
+        alert("Presiona aceptar para continuar con la operacion.");
     }
-    if (ingreso == "1" || ingreso == "uno" || ingreso == "UNO" ) {
+    else if (ingreso2 == "1" || ingreso2 == "uno" || ingreso2 == "UNO" ) {
         
         let resultadoGene1 = envio + general;
         parseFloat(alert("Seleccionaste 1 Entrada Vip, valor mas envio: " + resultadoGene1));
-        console.log(resultadoGene1);
+        console.log("valor mas envio: " + resultadoGene1);
 
-    }else if (ingreso == "2" || ingreso == "dos" || ingreso == "DOS") {
+    }else if (ingreso2 == "2" || ingreso2 == "dos" || ingreso2 == "DOS") {
         
         let resultadoGene2 = envio + general * 2;
         parseFloat(alert("Seleccionaste 2 Entradas Vip, valor mas envio: " + resultadoGene2));
-        console.log(resultadoGene2);
+        console.log("valor mas envio: " + resultadoGene2);
 
-    } else if (ingreso == "3" || ingreso == "dos" || ingreso == "TRES") {
+    } else if (ingreso2 == "3" || ingreso2 == "dos" || ingreso2 == "TRES") {
         
         let resultadoGene3 = envio + general * 3;
         parseFloat(alert("Seleccionaste 3 Entradas General, valor mas envio: " + resultadoGene3))
-        console.log(resultadoGene3);
+        console.log("valor mas envio: " + resultadoGene3);
     }else{
         alert("No seleccionaste ninguna opcion.")
-    }   
+        break;
+    }
     
+    
+
+    const bebidas = [
+        { id: 1, nombre: "fernet", precio: 800},
+        { id: 2, nombre: "vodka", precio: 800},
+        { id: 3, nombre: "cerveza", precio: 800},
+        { id: 4, nombre: "campari", precio: 800},
+        { id: 5, nombre: "agua", precio: 200},
+        { id: 6, nombre: "coca cola", precio: 500},
+        { id: 7, nombre: "sprite", precio: 500},
+        { id: 8, nombre: "agua saborizada", precio: 200},
+        ];
+        
+        console.table(bebidas);
+
+
 
 function dato2(direccion) {
     console.log("Tu entrada sera enviada a este domicilio  " + direccion + " /Demora estimada 5 dias/");
 } 
 
-
 direccion = prompt("Direccion donde se enviaran las entradas");
 
 dato2(direccion);
 
+
+
+
+const datosPersonales = {
+    NombreCompleto : nombreApellido,
+    entradasVip: ingreso,
+    entradasGenerales: ingreso2,
+    direccionCorreo: email,
+    direcionCompleta: direccion,
+    
+}
+console.table(datosPersonales);
+
+
+
+
+
+
+//arreglo de precios
+const precioActualizado = bebidas.map(el=>{
+    return{
+        id: el.id,
+        nombre: el.nombre,
+        precios: el.precio * 1.5 
+        };
+    });
+    
+    console.table(precioActualizado);
+
+for (const element of precioActualizado) {
+    console.log(element);
+    
+}
+alert("Muchas gracias por su compra")
 }
